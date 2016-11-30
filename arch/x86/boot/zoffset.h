0213c82f61d2e8710376ad36d7acf2ec41d6d16d
Felipe Viglioni Goncalves Ribeiro <ra106665@dostoevsky.lab.ic.unicamp.br>
2016-11-29 22:35:07 -0200

    rs

--- arch/x86/boot/zoffset.h
+++ arch/x86/boot/zoffset.h
@@ -1,7 +1,7 @@
 #define ZO__ehead 0x00000069
 #define ZO__end 0x00712000
-#define ZO__text 0x0030e304
+#define ZO__text 0x0030e2d8
 #define ZO_input_data 0x00000069
 #define ZO_startup_32 0x00000000
-#define ZO_z_input_len 0x0030e29b
+#define ZO_z_input_len 0x0030e26c
 #define ZO_z_output_len 0x006025b8
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.8.6"
+#define UTS_RELEASE "4.8.6-ga29c684-dirty"
